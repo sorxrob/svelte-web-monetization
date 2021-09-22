f467084b49e5c18a62dbc009c2036d42a816e728
Robert Soriano <sorianorobertc@gmail.com>
2021-09-22 22:48:31 +0800

    Update deps

--- example/src/components/Nav.svelte
+++
@@ -1,5 +0,0 @@
-<nav class="white" role="navigation">
-  <div class="nav-wrapper container">
-    <a id="logo-container" href="#" class="brand-logo black-text">Mediocre</a>
-  </div>
-</nav>
--- src/Component.svelte
+++ src/Component.svelte
@@ -1,36 +1,48 @@
 <script>
-  import { onMount, createEventDispatcher } from "svelte";
-  const dispatch = createEventDispatcher();
-
-  let isLoading = true;
-  let isMonetized = false;
-
+  import { onDestroy, onMount, createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+  
+  let isLoading = true
+  let isMonetized = true
+  
+  const monetizationEventCb = (ev) => {
+      const eventName = ev.type.replace('monetization', '')
+      dispatch(eventName, ev.detail)
+  
+      if (ev.type === 'monetizationstart') {
+          isLoading = false
+          isMonetized = true
+      }
+  }
+  
   onMount(() => {
-    if (!document.monetization) {
-      isLoading = false;
-      isMonetized = false;
-      return;
-    }
-
-    const { state } = document.monetization;
-
-    if (state === "stopped") {
-      isLoading = false;
-      isMonetized = false;
-    }
-
-    const events = ["start", "progress", "pending", "stop"];
-    events.forEach(name => {
-      document.monetization.addEventListener("monetization" + name, event => {
-        dispatch(name, event.detail);
-
-        if (name === "start") {
-          isLoading = false;
-          isMonetized = true;
-        }
-      });
-    });
-  });
-</script>
-
-<slot {isLoading} {isMonetized} />
+      if (!document.monetization) {
+          isLoading = false
+          isMonetized = false
+          return
+      }
+  
+      const { state } = document.monetization
+  
+      if (state === 'stopped') {
+          isLoading = false
+          isMonetized = false
+      }
+  
+      document.monetization.addEventListener('monetizationstart', monetizationEventCb)
+      document.monetization.addEventListener('monetizationstop', monetizationEventCb)
+      document.monetization.addEventListener('monetizationprogress', monetizationEventCb)
+      document.monetization.addEventListener('monetizationpending', monetizationEventCb)
+  })
+  
+  onDestroy(() => {
+      if (!document.monetization) return
+  
+      document.monetization.removeEventListener('monetizationstart', monetizationEventCb)
+      document.monetization.removeEventListener('monetizationstop', monetizationEventCb)
+      document.monetization.removeEventListener('monetizationprogress', monetizationEventCb)
+      document.monetization.removeEventListener('monetizationpending', monetizationEventCb)
+  })
+  </script>
+  
+  <slot {isLoading} {isMonetized} />
